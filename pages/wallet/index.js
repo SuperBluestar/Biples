905e132a2a2794dad1a0a4ffac2df4de101b2079
highshot-rk <sagarn62018@gmail.com>
2022-08-02 21:57:41 +0900

    init

---
+++ pages/wallet/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Wallet() {
+  return (
+    <div>
+        <div className='grid grid-cols-5 gap-4'>
+            <div className=''><></>Back</div>
+            <div className='col-span-4'>
+                <div>Connect Your Wallet</div>
+                <div>wallet</div>
+                <div>reminder</div>
+                <div>how to create</div>
+            </div>
+        </div>
+    </div>
+  )
+}
